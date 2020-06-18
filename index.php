60ab5e38fd6faa87253137b495218e839d253ad7
Edipo <edipo91@outlook.com>
2020-06-18 18:11:30 -0400

    inserir e alterar

--- index.php
+++ index.php
@@ -20,9 +20,37 @@ $search = Usuario::search("jo");
 echo json_encode($search);
 */
 
-//Carrega um usuário usando o login e a senha
+/*Carrega um usuário usando o login e a senha
 $usuario = new Usuario();
 $usuario->login("Maria", "4567");
 echo $usuario;
+*/
 
+/* id:1
+$aluno = new Usuario();
+
+$aluno->setDeslogin("Cleuza");
+$aluno->setDessenha("c1v2v3");
+
+$aluno->insert();
+
+echo $aluno;
+*/
+/* ou usar este metódo debaixo, insere usuario no db */
+/*este é usado com __construct*/
+//aqui em baixo estou criando um novo usuario
+/*$aluno = new Usuario("Bruna","cvb123");
+
+$aluno->insert();
+
+echo $aluno;
+*/
+
+$usuario = new Usuario();
+
+$usuario->loadById(10);
+
+$usuario->update("Bicicleta", "14sa20");
+
+echo $usuario;
 ?>
